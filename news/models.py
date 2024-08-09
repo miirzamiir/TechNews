63e8ce053f720a09c8d3fca0b90c55f4af58e9e4
miirzamiir <amirmohamadshakuri@gmail.com>
2024-08-10 01:08:58 +0330

    added news and tag models.

--- news/models.py
+++ news/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    tag_label = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.tag_label
+
+
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField(null=True)
+    tags = models.ManyToManyField(to=Tag)
+    resource = models.URLField(blank=False, unique=True)
+
+    def __str__(self) -> str:
+        return self.title
